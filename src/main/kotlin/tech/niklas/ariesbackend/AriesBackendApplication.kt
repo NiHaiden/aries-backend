456b634bc7c5284878f99bdb256688e218c3a7cf
Niklas Haiden <niklas.haiden@gmail.com>
2023-06-12 22:17:53 +0200

    initial-commit

---
+++ src/main/kotlin/tech/niklas/ariesbackend/AriesBackendApplication.kt
@@ -0,0 +1,11 @@
+package tech.niklas.ariesbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AriesBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<AriesBackendApplication>(*args)
+}
---
+++ src/test/kotlin/tech/niklas/ariesbackend/AriesBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.niklas.ariesbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AriesBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
